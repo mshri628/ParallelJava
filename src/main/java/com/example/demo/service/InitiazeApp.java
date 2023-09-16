481f8a1803fcc1aa64cb11af546dfa4228db67df
mohit.shrivastava <mohit.shrivastava@publicissapient.com>
2023-09-16 14:19:09 +0400

    Added code formatting and comments

--- src/main/java/com/example/demo/service/InitiazeApp.java
+++ src/main/java/com/example/demo/service/InitiazeApp.java
@@ -5,7 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
@@ -18,43 +19,18 @@ public class InitiazeApp {
     private ApiProxy apiProxy;
 
 
+    //Entry point to run any example
     public void runApp() throws ExecutionException, InterruptedException {
         getAndMergeApi(5);
     }
 
 
-    private void getAndMergeThreeApi() throws ExecutionException, InterruptedException{
-        log.info("Execution started calling 3 api API 1 and API 2 and API 3");
-
-        long startTime = System.currentTimeMillis();
-        CompletableFuture api1ResponseCompletable  = CompletableFuture.runAsync(() -> {
-            try {
-                getAndMergeTwoApi();
-            } catch (ExecutionException e) {
-                e.printStackTrace();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-        CompletableFuture<String> api2ResponseCompletable  = CompletableFuture.supplyAsync(() ->apiProxy.api3("api2"));
-        CompletableFuture<Void> combinedFuture = CompletableFuture.allOf(api1ResponseCompletable, api2ResponseCompletable);
-
-        // Wait for both API calls to complete
-        combinedFuture.join();
-
-        String api2Result = api2ResponseCompletable.get();
-        long endTime = System.currentTimeMillis();
-        log.info("Execution Complete for API 1 API 2 API 3 Time -{}",endTime-startTime);
-
-
-
-    }
     // Example 1
     private void getAndMergeTwoApi() throws ExecutionException, InterruptedException {
         log.info("Execution started calling two api API 1 and API 2");
         long startTime = System.currentTimeMillis();
-        CompletableFuture<String> api1ResponseCompletable  = CompletableFuture.supplyAsync(() ->apiProxy.api1("api1"));
-        CompletableFuture<String> api2ResponseCompletable  = CompletableFuture.supplyAsync(() ->apiProxy.api2("api2"));
+        CompletableFuture<String> api1ResponseCompletable = CompletableFuture.supplyAsync(() -> apiProxy.api1("api1"));
+        CompletableFuture<String> api2ResponseCompletable = CompletableFuture.supplyAsync(() -> apiProxy.api2("api2"));
 
         CompletableFuture<Void> combinedFuture = CompletableFuture.allOf(api1ResponseCompletable, api2ResponseCompletable);
 
@@ -65,16 +41,46 @@ public class InitiazeApp {
         String api1Result = api1ResponseCompletable.get();
         String api2Result = api2ResponseCompletable.get();
         long endTime = System.currentTimeMillis();
-        log.info("Execution Complete for two api API 1 and API 2 Time -{}",endTime-startTime);
+        log.info("Execution Complete for two api API 1 and API 2 Time -{}", endTime - startTime);
 
     }
 
+    //Example 2
+    private void getAndMergeThreeApi() throws ExecutionException, InterruptedException {
+        log.info("Execution started calling 3 api API 1 and API 2 and API 3");
+
+        long startTime = System.currentTimeMillis();
+        CompletableFuture api1ResponseCompletable = CompletableFuture.runAsync(() -> {
+            try {
+                getAndMergeTwoApi();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        CompletableFuture<String> api3ResponseCompletable = CompletableFuture.supplyAsync(() -> apiProxy.api3("api2"));
+        CompletableFuture<Void> combinedFuture = CompletableFuture.allOf(api1ResponseCompletable, api3ResponseCompletable);
+
+        // Wait for both API calls to complete
+        combinedFuture.join();
+
+        String api3Result = api3ResponseCompletable.get();
+        long endTime = System.currentTimeMillis();
+        log.info("Execution Complete for API 1 API 2 API 3 Time -{}", endTime - startTime);
+
+
+    }
+
+
+    //Example 3
     private void getAndMergeTwoApiWithException() throws ExecutionException, InterruptedException {
         log.info("Execution started calling two api API 1 and API 4");
         long startTime = System.currentTimeMillis();
-        CompletableFuture<String> api1ResponseCompletable  = CompletableFuture.supplyAsync(() ->apiProxy.api1("api1"));
-        CompletableFuture<String> api4ResponseCompletable  = CompletableFuture.supplyAsync(() ->apiProxy.api4("api4")).exceptionally(ex ->{
-            log.error("Api 4 threw  exception msg-{} ",ex.getMessage());
+
+        CompletableFuture<String> api1ResponseCompletable = CompletableFuture.supplyAsync(() -> apiProxy.api1("api1"));
+        CompletableFuture<String> api4ResponseCompletable = CompletableFuture.supplyAsync(() -> apiProxy.api4("api4")).exceptionally(ex -> {
+            log.error("Api 4 threw  exception msg-{} ", ex.getMessage());
             return "Fallback Response";
         });
 
@@ -87,18 +93,19 @@ public class InitiazeApp {
         String api1Result = api1ResponseCompletable.get();
         String api4Result = api4ResponseCompletable.get();
         long endTime = System.currentTimeMillis();
-        log.info("Execution Complete for two api API 1 and API 4 Time -{}",endTime-startTime);
-        log.info("api1Result -{} api4Result-{} ",api1Result,api4Result);
+        log.info("Execution Complete for two api API 1 and API 4 Time -{}", endTime - startTime);
+        log.info("api1Result -{} api4Result-{} ", api1Result, api4Result);
 
     }
 
+    //Example 4
     private void getAndMergeApi(Integer n) throws ExecutionException, InterruptedException {
-        log.info("Execution started calling {} api API",n);
+        log.info("Execution started calling {} api API", n);
         long startTime = System.currentTimeMillis();
 
         List<CompletableFuture<String>> futureList = new ArrayList<>();
         for (Integer i = 1; i <= n; i++) {
-            String  param = i.toString();
+            String param = i.toString();
             CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> apiProxy.api(param));
             futureList.add(future);
         }
@@ -106,25 +113,19 @@ public class InitiazeApp {
         // Wait for all CompletableFuture to complete and collect the results
         List<Integer> resultList = new ArrayList<>();
         for (Integer i = 1; i <= n; i++) {
-            String result = futureList.get(i-1).get();
-            log.info("API RESPONSE FOR PARAM {} is {}",i,result);
+            String result = futureList.get(i - 1).get();
+            log.info("API RESPONSE FOR PARAM {} is {}", i, result);
         }
         long endTime = System.currentTimeMillis();
-        log.info("Execution Complete for {} api  Time -{}",n,endTime-startTime);
+        log.info("Execution Complete for {} api  Time -{}", n, endTime - startTime);
 
     }
 
 
-    private String handleException(Exception e){
-        log.error("Exception msg-{}",e.getMessage());
+    private String handleException(Exception e) {
+        log.error("Exception msg-{}", e.getMessage());
         return "Fallback Value";
     }
-
-
-
-
-
-
 
 
 }
